953813c02f91c2b2fc3df108a352c65db1701a78
acadapetala <carlosneto68123@gmail.com>
2026-05-21 17:07:16 -0300

    Update frete.js

--- api/frete.js
+++ api/frete.js
@@ -1,43 +1,5 @@
-export default async function handler(req, res) {
-
-  const response = await fetch(
-    'https://www.melhorenvio.com.br/api/v2/me/shipment/calculate',
-    {
-      method: 'POST',
-
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.MELHOR_ENVIO_TOKEN}`,
-        'Accept': 'application/json'
-      },
-
-      body: JSON.stringify({
-
-        from: {
-          postal_code: '19470000'
-        },
-
-        to: {
-          postal_code: req.query.cep
-        },
-
-        products: [
-          {
-            id: "1",
-            width: 20,
-            height: 10,
-            length: 30,
-            weight: 1,
-            insurance_value: 59.90,
-            quantity: 1
-          }
-        ]
-
-      })
-    }
-  );
-
-  const data = await response.json();
-
-  res.status(200).json(data);
+export default function handler(req, res) {
+  res.status(200).json({
+    funcionando: true
+  });
 }
